8ebe69d260db81fbeca16dd875be4238d9275f51
BrunoLicuadri <blicuadri@gmail.com>
2023-01-31 13:01:26 -0300

    Starting Crud Client challenge with H2 db set and entities.Client created.

---
+++ src/main/java/com/godknows/challengeCrudClient/ChallengeCrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.godknows.challengeCrudClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCrudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godknows/challengeCrudClient/ChallengeCrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godknows.challengeCrudClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
