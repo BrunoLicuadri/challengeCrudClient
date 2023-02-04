aaf3050df24706872cc21f7f3953639c7aa65f30
BrunoLicuadri <blicuadri@gmail.com>
2023-02-04 04:00:44 -0300

    Created method findAll pageable as well as CRUD functions

--- src/main/java/com/godknows/challengeCrudClient/controllers/ClientController.java
+++ src/main/java/com/godknows/challengeCrudClient/controllers/ClientController.java
@@ -7,11 +7,13 @@ import com.godknows.challengeCrudClient.repositories.ClientRepository;
 import com.godknows.challengeCrudClient.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 
@@ -23,9 +25,35 @@ public class ClientController {
     private ClientService cliService;
 
     @GetMapping(value="/{id}")
-    public ClientDTO dto (@PathVariable Long id){
+    public ResponseEntity<ClientDTO> findById (@PathVariable Long id){
         ClientDTO dto = cliService.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        Page<ClientDTO> dto = cliService.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert (@RequestBody ClientDTO dto){
+        dto = cliService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<ClientDTO> update (@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = cliService.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id){
+        cliService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/java/com/godknows/challengeCrudClient/services/ClientService.java
+++ src/main/java/com/godknows/challengeCrudClient/services/ClientService.java
@@ -4,6 +4,8 @@ import com.godknows.challengeCrudClient.dto.ClientDTO;
 import com.godknows.challengeCrudClient.entities.Client;
 import com.godknows.challengeCrudClient.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,43 @@ public class ClientService {
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll (Pageable pageable){
+        Page<Client> result = cliRepo.findAll(pageable);
+        return result.map(x-> new ClientDTO(x));
+    }
+
+
+    @Transactional
+    public ClientDTO insert (ClientDTO dto){
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = cliRepo.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update (Long id, ClientDTO dto){
+        Client entity = cliRepo.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = cliRepo.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete (Long id){
+        cliRepo.deleteById(id);
+    }
+
+
+    private void copyDtoToEntity (ClientDTO dto, Client entity){
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 
 }
